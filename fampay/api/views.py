7dfc727b3222d29b2eac686cf5b4a2175d66f96b
Vaibhav Garg <vaibh.garg.3010@gmail.com>
2022-07-07 19:08:15 +0530

    Rework

--- fampay/api/views.py
+++ fampay/api/views.py
@@ -7,64 +7,76 @@ from django.db.models import Q
 import asyncio
 from django.core.paginator import Paginator ,EmptyPage ,PageNotAnInteger
 from asgiref.sync import sync_to_async
+import time
 # Create your views here.
 
 
-@sync_to_async
-def fetch_videos(x):
-     return redirect('home')    
+# @sync_to_async
+async def fetch_videos():
+    
+        search_url='https://www.googleapis.com/youtube/v3/search'
+        video_url = 'https://www.googleapis.com/youtube/v3/videos'
+            
+        search_params = {
+                    'part' : 'snippet',
+                    'q'  : 'cricket', 
+                    'key' : settings.YOUTUBE_DATA_API_KEY,
+                    'maxResults' : 10,
+                    'type' : 'video'
+                }
+        video_ids=[]
+        r=requests.get(search_url,params=search_params)
+            
+            # print(r.text)
+        print(r.json()['items'][0]['id']['videoId'])
+        
+        
+        
+            #querying the videos with thier id
+        res=r.json()['items']
+        for result in res:
+                video_ids.append(result['id']['videoId'])
+
+        video_params = {
+                    'key' : settings.YOUTUBE_DATA_API_KEY,
+                    'part' : 'snippet,contentDetails',
+                    'id' : ','.join(video_ids),
+                    'maxResults' : 10
+                }
+
+                #querying detail infromation about the videos
+        r = requests.get(video_url, params=video_params)
+            # print(r.text)
+        results =  r.json()['items']
+        for result in results:
+                    video_data = Video(
+                        
+                        video_id = result['id'],
+                        title = result['snippet']['title'],
+                        description = result['snippet']['description'],
+                        url = f'https://www.youtube.com/watch?v={ result["id"] }',
+                        upload_time = result['snippet']['publishedAt'],
+                        thumbnail = result['snippet']['thumbnails']['high']['url']
+                    )
+
+                    print(video_data)
+                    
+                    video_data.save()
+                    return redirect('home')    
 
 # Creating Function which will fetch data from api and store it in databas
 # Sending data to the index temmplate
-async def home(request):
-    
-    search_url='https://www.googleapis.com/youtube/v3/search'
-    video_url = 'https://www.googleapis.com/youtube/v3/videos'
-        
-    search_params = {
-                'part' : 'snippet',
-                'q' :request.POST.get('search'), 
-                'key' : settings.YOUTUBE_DATA_API_KEY,
-                'maxResults' : 10,
-                'type' : 'video'
-            }
-    video_ids=[]
-    r=requests.get(search_url,params=search_params)
-        
-        # print(r.text)
-        # print(r.json()['items'][0]['id']['videoId'])
-       
-       
-       
-        #querying the videos with thier id
-    res=r.json()['items']
-    for result in res:
-            video_ids.append(result['id']['videoId'])
+def home(request):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main1(loop))
+    loop.close()
+    # f1 = loop.create_task(fetch_videos())
+    # await asyncio.wait([f1])
 
-    video_params = {
-                'key' : settings.YOUTUBE_DATA_API_KEY,
-                'part' : 'snippet,contentDetails',
-                'id' : ','.join(video_ids),
-                'maxResults' : 10
-            }
-
-              #querying detail infromation about the videos
-    r = requests.get(video_url, params=video_params)
-        # print(r.text)
-    results =  r.json()['items']
-    for result in results:
-                video_data = Video(
-                    
-                    video_id = result['id'],
-                    title = result['snippet']['title'],
-                    description = result['snippet']['description'],
-                    url = f'https://www.youtube.com/watch?v={ result["id"] }',
-                    upload_time = result['snippet']['publishedAt'],
-                    thumbnail = result['snippet']['thumbnails']['high']['url']
-                )
-                
-                video_data.save()  
-    
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(fetch_videos())
+    # loop.close()
     
 #it will give a html response based on the query
     item = []
@@ -86,10 +98,23 @@ async def home(request):
     context = {
     'videos' : page
     }
+ 
     return render(request, 'index.html', context)
-
-
-
 
 # def index(req):
 #     return render(req,'index.html')
+
+async def function_asyc():
+    
+    for i in range(100000):
+        if i % 50000 == 0:
+            print("Hello, I'm Vaibhav")
+              
+            # New Line Added
+            await asyncio.sleep(0.01)
+    return 0
+
+async def main1(loop):
+    f1 = loop.create_task(fetch_videos())
+    f2 = loop.create_task(function_asyc())
+    await asyncio.wait([f1, f2])
